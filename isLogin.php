17b84c6bd2a776f5df497fbd60156a7f4b658bff
Wivine-Bdx <wivine.boidoux@free.fr>
2024-06-04 14:05:21 +0200

    Ajout de la fonction d'autorisation

---
+++ isLogin.php
@@ -0,0 +1,21 @@
+<?php
+// Création d'une fonction d'autorisation
+function isLoggedIn()
+{
+// Connexion à la BDD
+    $pdo = require_once './database.php';
+// Récupération de l'id de la session aux cookies
+    $sessionId = $_COOKIE['session'] ?? '';
+// Si id présent, requête pour récupérer la session (id session et id user)
+    if ($sessionId) {
+            $sessionUserStatement = $pdo->prepare('SELECT * FROM session JOIN user on user.id=session.userid WHERE session.id=?');
+            $sessionUserStatement->execute([$sessionId]);
+            $user = $sessionUserStatement->fetch();
+}
+// Si user n'est pas null ni false alors on retourne le user sinon false
+if ($user !== null && $user !== false) {
+    return $user;
+} else {
+    return false;
+}
+}
